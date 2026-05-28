b0fbfbaa2258c0351da6b8cde389b6b31478cfe2
yelsindatu <yelsin24@mhs.unitama.ac.id>
2026-05-28 04:11:21 -0700

    feat: implementasi fitur read untuk pasien

--- resources/views/pasien/index.blade.php
+++ resources/views/pasien/index.blade.php
@@ -3,11 +3,13 @@
     <x-slot:title>{{ $title }}</x-slot>
 
     <div class="d-flex justify-content-between mb-4">
+
         <h2 class="fw-bold">Data Pasien</h2>
 
         <a href="/pasien/create" class="btn btn-primary">
             Tambah Pasien
         </a>
+
     </div>
 
     <ul class="list-group">
@@ -17,11 +19,13 @@
 
                 {{ $loop->iteration }}.
 
-                {{ $pasien->nama_pasien }} -
+                {{ $pasien->nama_pasien }} --
 
-                {{ $pasien->umur }} Tahun -
+                {{ $pasien->umur }} Tahun --
 
-                {{ $pasien->jenis_kelamin }} -
+                {{ $pasien->jenis_kelamin }} --
+
+                {{ $pasien->alamat }} --
 
                 {{ $pasien->keluhan }}
 
